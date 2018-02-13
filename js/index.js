a9aa6afcfc825e82d114f5efc0f5d9d23868b52e
Muhammad <muhammadraza0047@gmail.com>
2018-02-13 16:05:18 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-consoleText(['Welcome.', 'To', 'Lets Talk Python','Want to Know More !!','Sign up @bit.ly/ltpnewbies','Join Our Slack Chat','@bit.ly/ltpslack'], 'text',['white']);
+consoleText(['Welcome.', 'to', 'Lets Talk Python','Want to Know More?','Sign up @bit.ly/ltpnewbies','Join us on Slack','@bit.ly/ltpslack'], 'text',['white']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
