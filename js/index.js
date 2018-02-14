889b7e9b043d32356bf5887c857c165334538f5c
popoway <31576265+popoway@users.noreply.github.com>
2018-02-14 09:02:35 -0500

    modify poem content

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-consoleText(['Welcome to Let\'s Talk Python','Want to know more?','Sign up @bit.ly/ltpnewbies','Join us on Slack','@bit.ly/ltpslack','Follow us on Twitter @LetsTalkPython','Connect With Us on LinkedIn @ Lets Talk Python'], 'text',['white']);
+consoleText(['From Ming <3  Feb/14/2018', 'Desire H','To Helena\:','H is a letter','with so many meanings —','heat from the hot tea,','hit when he’s late to meet.','Health across head to feet,','and keep hope as a guarantee.','Desire is a word','stands for strong wishing —','holding hands if wind’s blowing,','hugging me when he’s suffering.','Enjoying the sweetest apple,','and lovely cheek nowhere to see.','Desire H is a string','connecting you and me.','Even the Wi-Fi is weak or poor,','powerful girl can always be sought.','Believe one day dreaming is there,','and even hearts will be odd.','From Ming <3  Feb/14/2018'], 'text',['white']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
